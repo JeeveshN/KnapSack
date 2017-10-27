1e99f6f2cb9ff268f62b2cac9f794ea943f127e9
JeeveshN <narang.jeevesh@gmail.com>
2017-10-28 01:21:21 +0530

    Berkley's Parser

---
+++ Parsers/Berkleys.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+import requests
+import time
+
+BASE_URL = "https://www2.eecs.berkeley.edu/Courses/CS/?_ga=2.44961508.1522982388.1509131899-838894781.1509131899"
+COURSES = []
+
+def get_courses():
+	doc = requests.get(BASE_URL)
+	soup = BeautifulSoup(doc.text,"html.parser")
+	content = soup.find("div","content")
+	course_li = content.find_all("li")
+	for course in course_li:
+		COURSES.append(course.contents[1].string)
+	with open('Berkley.txt','w')as f:f.write("\n".join(COURSES).strip('\n'))
+
+
+
+if __name__ == "__main__":
+	get_courses()
